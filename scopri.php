927ec43234db1495f507dafe6e21b368ad6ad8c2
Nikoooo14 <158780435+Nikoooo14@users.noreply.github.com>
2024-03-19 17:21:55 +0100

    scopri.php

--- scopri.php
+++ scopri.php
@@ -1,6 +1,6 @@
 <?php
-$nome=$_POST["nome"];
-$eta=$_POST["eta"];
+$nome=$_GET["nome"];
+$eta=$_GET["eta"];
 $gay="marco";
 $etaz=20;
 if (stripos($nome, $gay) !== false && strpos($eta, (string)$etaz) !== false) {
